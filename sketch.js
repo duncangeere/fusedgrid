eb2e4c64f8291591e011e25dac40b920ab932210
Duncan Geere <radio.edit@gmail.com>
2020-08-12 14:49:09 +0200

    Initial commit

---
+++ sketch.js
@@ -0,0 +1,18 @@
+// A4
+var r = new Rune({
+    container: "body",
+    width: 1123,
+    height: 794,
+    debug: true
+});
+
+function graphic() {
+
+    // CODE GOES HERE
+    r.circle(10,10,20,20)
+
+}
+
+// Draw it 
+graphic();
+r.draw();
